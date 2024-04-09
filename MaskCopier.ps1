5b48d1353b6c792c72b55844fe934af74857f279
Oliver Everard <53011298+OliverEverard@users.noreply.github.com>
2024-04-09 16:43:18 +0100

    Create MaskCopier.ps1

---
+++ MaskCopier.ps1
@@ -0,0 +1,10 @@
+copy MVI_0008.png MVI_0024.png
+copy MVI_0008.png MVI_0025.png
+copy MVI_0008.png MVI_0026.png
+copy MVI_0008.png MVI_0027.png
+copy MVI_0008.png MVI_0028.png
+copy MVI_0008.png MVI_0029.png
+copy MVI_0008.png MVI_0030.png
+copy MVI_0008.png MVI_0031.png
+
+
